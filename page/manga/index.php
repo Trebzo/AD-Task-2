d09a3903463d1869660dc433e751a8fd361867c2
Ozbert <202310393@fit.edu.ph>
2025-05-20 00:29:06 +0800

    fix(manga index) - Refactor index.php structure for improved layout and organization

--- page/manga/index.php
+++ page/manga/index.php
@@ -1,35 +1,48 @@
-<?php include '../../components/header.component.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manga Picks | Anime Planet</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php include '../../components/header.component.php'; ?>
 
-<main>
-    <section class="banner">
-        <h2>Discover trending manga</h2>
-        <p>Explore top manga stories and start your collection.</p>
-    </section>
+        <main>
+            <section class="banner manga-banner">
+                <h2>Discover trending manga</h2>
+                <p>Explore top manga stories and start your collection.</p>
+            </section>
 
-    <section class="recommended">
-        <h3>Top Manga Picks</h3>
-        <div class="card-container">
-            <?php
-                $mangaList = [
-                    ["title" => "One Piece", "img" => "../../assets/img/manga1.jpg"],
-                    ["title" => "Chainsaw Man", "img" => "../../assets/img/manga2.jpg"],
-                    ["title" => "Jujutsu Kaisen", "img" => "../../assets/img/manga3.jpg"],
-                    ["title" => "Oshi no Ko", "img" => "../../assets/img/manga4.jpg"]
-                ];
+            <section class="recommended">
+                <h3>Top Manga Picks</h3>
+                <div class="card-container">
+                    <?php
+                        $mangaList = [
+                            ["title" => "One Piece", "img" => "assets/img/one.jpg"],
+                            ["title" => "Chainsaw Man", "img" => "assets/img/chain.jpg"],
+                            ["title" => "Jujutsu Kaisen", "img" => "assets/img/juju.jpg"],
+                            ["title" => "Oshi no Ko", "img" => "assets/img/oshi.jpg"],
+                            ["title" => "Hajime No Ippo The Fighting", "img" => "assets/img/ippo.webp"],
+                        ];
 
-                function displayManga($manga) {
-                    echo '<div class="card">';
-                    echo '<img src="' . $manga["img"] . '" alt="' . $manga["title"] . '">';
-                    echo '<p class="title">' . $manga["title"] . '</p>';
-                    echo '</div>';
-                }
+                        function displayManga($manga) {
+                            echo '<div class="card">';
+                            echo '<img src="' . $manga["img"] . '" alt="' . $manga["title"] . '">';
+                            echo '<p class="title">' . $manga["title"] . '</p>';
+                            echo '</div>';
+                        }
 
-                foreach ($mangaList as $manga) {
-                    displayManga($manga);
-                }
-            ?>
-        </div>
-    </section>
-</main>
+                        foreach ($mangaList as $manga) {
+                            displayManga($manga);
+                        }
+                    ?>
+                </div>
+            </section>
+        </main>
 
-<?php include '../../components/footer.component.php'; ?>
+        <?php include '../../components/footer.component.php'; ?>
+    </div>
+</body>
+</html>
