db00231e89857117289af130c3faa40d1206127b
Ozbert <202310393@fit.edu.ph>
2025-05-19 23:07:17 +0800

    fix(header) - added missing home link

--- components/header.component.php
+++ components/header.component.php
@@ -6,12 +6,15 @@
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <h1 class="logo">anime<span>_planet</span></h1>
+    <header class="site-header">
+    <div class="container">
+        <h1 class="logo">anime<span>planet</span></h1>
+        <nav class="main-nav">
             <ul>
+                <li><a href="../../index.php">Home</a></li>
                 <li><a href="/page/anime/index.php">Anime</a></li>
                 <li><a href="/page/manga/index.php">Manga</a></li>
             </ul>
         </nav>
-    </header>
+    </div>
+</header>
